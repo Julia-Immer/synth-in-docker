18c96238d5cbdcabacd93bf141c301d9013168a8
Julia Scott Immer <92125966+Julia-Immer@users.noreply.github.com>
2023-01-25 20:49:08 -0700

    removed out, incorrect message about integer

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import synth
 
 print("running synth app - dummy version")
 
-frequency = input("Input a frequency you want to hear. Must be integer : ")
+frequency = input("Input a frequency you want to hear: ")
 
 frequency = float(frequency)
 
